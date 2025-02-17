ef96ceb54fcf3bb10dbe4a82a028210286028095
arman-devops7 <armanmukadamdevops@gmail.com>
2025-02-17 22:03:43 +0530

    added variables.js

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,12 @@
+const accountId = '1443'
+let accountEmail = "hello@example.com"
+
+// never use var
+var accountPassword = "password"
+
+// dont use like this also
+accountCity = "jaipur"
+
+let accountState;
+
+console.log(accountState)
